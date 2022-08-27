93e580b3ee6419df70e50b1da65c5496a80f7d9b
sourav4125 <souravmisra4125@gmail.com>
2022-08-27 23:34:46 +0530

    day-5 Added

--- login.js
+++ login.js
@@ -1,9 +1,13 @@
-function validate(){
+document.getElementById("box").addEventListener("submit", validate);
+function validate(event){
+   event.preventDefault();
     let username=document.getElementById("username").value;
     let password=document.getElementById("password").value;
 
     if(username=="sourav"&&password=="masai"){
+        console.log("HERE")
         alert("login successful");
+        window.location.href="head.html";
 
     }else{
         alert("login failed");
